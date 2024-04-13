b035c713c3eecffdfae29d3c18f99b3c7db2a242
LezinPlayz <166490526+LezinPlayz@users.noreply.github.com>
2024-04-12 23:52:18 -0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Lezin Playz</span> It's very painful to get rid of obesity. It hinders His Offices to Vonsecter Adipisicing Elit. With that pleasure, the times are blinded by the fact that everyone is chosen, they succeed, and as if they were to assume an opening! <br><br>Although it may be very painful, it will help to achieve adipisiscing Elit. From Beata Criticizes the Most Worthy Errors of Life, I Will Explain to the Inventor Why He Was Chosen for Great Offices? Lorem ipsum dolor sit amet, consectetur."
+      ],
+      typeSpeed: 20,
+      contentType: 'html'
+    });
+  }, 100);
